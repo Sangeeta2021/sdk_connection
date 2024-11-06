affed0aba6b95d94473cda06576a37858307296d
Sangeeta2021 <sangeeta@pravaahconsulting.com>
2024-11-06 11:43:36 +0530

    Code changes

--- android/app/src/main/java/MainActivity.java
+++ android/app/src/main/java/MainActivity.java
@@ -1,5 +1,139 @@
 
 
+// package com.example.ble_sdk_connection;
+
+// import android.os.Bundle;
+// import android.util.Log;
+// import androidx.annotation.NonNull;
+// import io.flutter.embedding.android.FlutterActivity;
+// import io.flutter.embedding.engine.FlutterEngine;
+// import io.flutter.plugin.common.MethodChannel;
+
+// import cn.icomon.icdevicemanager.ICDeviceManager;
+// import cn.icomon.icdevicemanager.ICDeviceManagerDelegate;
+// import cn.icomon.icdevicemanager.callback.ICScanDeviceDelegate;
+// import cn.icomon.icdevicemanager.model.data.ICWeightData;
+// import cn.icomon.icdevicemanager.model.device.ICDevice;
+// import cn.icomon.icdevicemanager.model.device.ICScanDeviceInfo;
+// import cn.icomon.icdevicemanager.model.other.ICConstant;
+// import cn.icomon.icdevicemanager.model.other.ICDeviceManagerConfig;
+
+// public class MainActivity extends FlutterActivity implements ICDeviceManagerDelegate, ICScanDeviceDelegate {
+//     private static final String CHANNEL = "com.example.ble_sdk_connection/ble";
+//     private MethodChannel methodChannel;
+//     private ICDeviceManager deviceManager;
+//     private ICDevice connectedDevice;
+
+//     @Override
+//     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+//         super.configureFlutterEngine(flutterEngine);
+//         methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
+
+//         methodChannel.setMethodCallHandler((call, result) -> {
+//             Log.d("MainActivity", "Method called: " + call.method);  // Debug log
+//             switch (call.method) {
+//                 case "initializeSDK":
+//                     initializeSDK();
+//                     result.success(null);
+//                     break;
+//                 case "startScan":
+//                     startScan();
+//                     result.success(null);
+//                     break;
+//                 case "stopScan":
+//                     stopScan();
+//                     result.success(null);
+//                     break;
+//                 case "connectDevice":
+//                     String macAddress = call.argument("macAddress");
+//                     connectToDevice(macAddress);
+//                     result.success(null);
+//                     break;
+//                 case "disconnectDevice":
+//                     disconnectDevice();
+//                     result.success(null);
+//                     break;
+//                 default:
+//                     result.notImplemented();
+//                     break;
+//             }
+//         });
+//     }
+
+//     private void initializeSDK() {
+//         deviceManager = ICDeviceManager.shared();
+//         deviceManager.setDelegate(this);
+//         ICDeviceManagerConfig config = new ICDeviceManagerConfig(getApplicationContext());
+//         deviceManager.initMgrWithConfig(config);
+//         Log.d("MainActivity", "SDK Initialized");
+//     }
+
+//     private void startScan() {
+//         Log.d("MainActivity", "Starting scan");
+//         deviceManager.startScanDevice();
+//     }
+
+//     private void stopScan() {
+//         deviceManager.stopScanDevice();
+//         Log.d("MainActivity", "Stopping scan");
+//     }
+
+//     private void connectToDevice(String macAddress) {
+//         if (connectedDevice == null) {
+//             connectedDevice = new ICDevice();
+//             connectedDevice.setMacAddr(macAddress);
+
+//             deviceManager.addDevice(connectedDevice, code -> 
+//                 Log.d("MainActivity", "Device connection status: " + code)
+//             );
+//         }
+//     }
+
+//     private void disconnectDevice() {
+//         if (connectedDevice != null) {
+//             deviceManager.removeDevice(connectedDevice, code -> 
+//                 Log.d("MainActivity", "Device disconnected with status: " + code)
+//             );
+//             connectedDevice = null;
+//         }
+//     }
+
+//     @Override
+//     public void onReceiveWeightData(ICDevice device, ICWeightData data) {
+//         if (data.isStabilized) {
+//             String weightData = "Weight: " + data.weight_kg + " kg";
+//             methodChannel.invokeMethod("onWeightDataReceived", weightData);
+//             Log.d("MainActivity", "Weight data received: " + weightData);
+//         }
+//     }
+
+//     @Override
+//     public void onScanResult(ICScanDeviceInfo deviceInfo) {
+//         String macAddress = deviceInfo.getMacAddr();
+//         methodChannel.invokeMethod("onDeviceFound", macAddress);
+//         Log.d("MainActivity", "Device found: " + macAddress);
+//     }
+
+//     @Override
+//     public void onDeviceConnectionChanged(ICDevice device, ICConstant.ICDeviceConnectState state) {
+//         Log.d("MainActivity", "Connection state changed: " + state);
+//     }
+
+//     @Override
+//     public void onInitFinish(boolean isSuccess) {
+//         Log.d("MainActivity", "SDK Initialization finished: " + isSuccess);
+//     }
+
+//     @Override
+//     public void onReceiveRSSI(ICDevice device, int rssi) {
+//         Log.d("MainActivity", "Received RSSI: " + rssi + " for device: " + device.getMacAddr());
+//     }
+// }
+
+
+
+//**************************************************Updated Code */
+
 package com.example.ble_sdk_connection;
 
 import android.os.Bundle;
@@ -24,41 +158,80 @@ public class MainActivity extends FlutterActivity implements ICDeviceManagerDele
     private ICDeviceManager deviceManager;
     private ICDevice connectedDevice;
 
+
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine);
-        methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
+    super.configureFlutterEngine(flutterEngine);
+    methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
 
-        methodChannel.setMethodCallHandler((call, result) -> {
-            Log.d("MainActivity", "Method called: " + call.method);  // Debug log
-            switch (call.method) {
-                case "initializeSDK":
-                    initializeSDK();
-                    result.success(null);
-                    break;
-                case "startScan":
-                    startScan();
-                    result.success(null);
-                    break;
-                case "stopScan":
-                    stopScan();
-                    result.success(null);
-                    break;
-                case "connectDevice":
-                    String macAddress = call.argument("macAddress");
-                    connectToDevice(macAddress);
-                    result.success(null);
-                    break;
-                case "disconnectDevice":
-                    disconnectDevice();
-                    result.success(null);
-                    break;
-                default:
-                    result.notImplemented();
-                    break;
-            }
-        });
-    }
+    // Register method call handler
+    methodChannel.setMethodCallHandler((call, result) -> {
+        Log.d("MainActivity", "Method called: " + call.method);
+        switch (call.method) {
+            case "initializeSDK":
+                initializeSDK();
+                result.success(null);
+                break;
+            case "startScan":
+                startScan();
+                result.success(null);
+                break;
+            case "stopScan":
+                stopScan();
+                result.success(null);
+                break;
+            case "connectDevice":
+                String macAddress = call.argument("macAddress");
+                connectToDevice(macAddress);
+                result.success(null);
+                break;
+            case "disconnectDevice":
+                disconnectDevice();
+                result.success(null);
+                break;
+            default:
+                result.notImplemented();
+                break;
+        }
+    });
+}
+
+
+    // @Override
+    // public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    //     super.configureFlutterEngine(flutterEngine);
+    //     methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
+
+    //     methodChannel.setMethodCallHandler((call, result) -> {
+    //         Log.d("MainActivity", "Method called: " + call.method);
+    //         switch (call.method) {
+    //             case "initializeSDK":
+    //                 initializeSDK();
+    //                 result.success(null);
+    //                 break;
+    //             case "startScan":
+    //                 startScan();
+    //                 result.success(null);
+    //                 break;
+    //             case "stopScan":
+    //                 stopScan();
+    //                 result.success(null);
+    //                 break;
+    //             case "connectDevice":
+    //                 String macAddress = call.argument("macAddress");
+    //                 connectToDevice(macAddress);
+    //                 result.success(null);
+    //                 break;
+    //             case "disconnectDevice":
+    //                 disconnectDevice();
+    //                 result.success(null);
+    //                 break;
+    //             default:
+    //                 result.notImplemented();
+    //                 break;
+    //         }
+    //     });
+    // }
 
     private void initializeSDK() {
         deviceManager = ICDeviceManager.shared();
@@ -129,8 +302,5 @@ public class MainActivity extends FlutterActivity implements ICDeviceManagerDele
         Log.d("MainActivity", "Received RSSI: " + rssi + " for device: " + device.getMacAddr());
     }
 }
-
-
-
 
 
--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -1,17 +1,20 @@
 
-
+// import 'package:ble_sdk_connection/utils/colors.dart';
+// import 'package:ble_sdk_connection/utils/constants.dart';
 // import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 
 // class MainScreen extends StatefulWidget {
+//   const MainScreen({super.key});
+
 //   @override
 //   _MainScreenState createState() => _MainScreenState();
 // }
 
 // class _MainScreenState extends State<MainScreen> {
 //   static const platform = MethodChannel("com.example.ble_sdk_connection/ble");
-//   String _bleData = "No data";
-//   List<String> _devices = [];
+//   String _weightData = "No data";
+//   List<String> _availableDevices = [];
 
 //   @override
 //   void initState() {
@@ -24,12 +27,12 @@
 //     switch (call.method) {
 //       case "onWeightDataReceived":
 //         setState(() {
-//           _bleData = call.arguments;
+//           _weightData = call.arguments;
 //         });
 //         break;
 //       case "onDeviceFound":
 //         setState(() {
-//           _devices.add(call.arguments);
+//           _availableDevices.add(call.arguments);
 //         });
 //         break;
 //     }
@@ -45,6 +48,7 @@
 
 //   Future<void> _startScan() async {
 //     try {
+//       _availableDevices.clear();
 //       await platform.invokeMethod("startScan");
 //     } catch (e) {
 //       print("Failed to start scan: $e");
@@ -62,29 +66,57 @@
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
-//       appBar: AppBar(title: Text("BLE Weight Machine")),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: <Widget>[
-//           Text("Weight Data: $_bleData"),
-//           ElevatedButton(
-//             onPressed: _startScan,
-//             child: Text("Start Scan"),
-//           ),
-//           ..._devices.map((device) => ListTile(
-//                 title: Text(device),
-//                 trailing: ElevatedButton(
-//                   onPressed: () => _connectToDevice(device),
-//                   child: Text("Connect"),
-//                 ),
-//               )),
-//         ],
+//       backgroundColor: bgColor,
+//       appBar: AppBar(
+//         centerTitle: true,
+//         backgroundColor: themeColor,
+
+//         title: Text("BLE Weight Tracker", style: appBarTitleStyle,)),
+//       body: Padding(
+//         padding: const EdgeInsets.symmetric(vertical:20,horizontal: 16 ),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text("Weight Data: $_weightData", style: blackTitleStyle),
+//             SizedBox(height: 20,),
+//             ElevatedButton(
+//               style: ElevatedButton.styleFrom(
+//                 backgroundColor: themeColor
+//               ),
+//               onPressed: _startScan,
+//               child: Padding(
+//                 padding: const EdgeInsets.symmetric(vertical: 4,horizontal: 12),
+//                 child: Text("Start Scan", style: buttonTextStyle,),
+//               ),
+//             ),
+//             Expanded(
+//               child: ListView.builder(
+//                 itemCount: _availableDevices.length,
+//                 itemBuilder: (context, index) {
+//                   return Padding(
+//                     padding: const EdgeInsets.only(bottom: 8),
+//                     child: ListTile(
+//                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),),
+//                       tileColor: wColor,
+//                       title: Text(_availableDevices[index]),
+//                       trailing: ElevatedButton(
+//                         style: ElevatedButton.styleFrom(backgroundColor: themeColor),
+//                         onPressed: () => _connectToDevice(_availableDevices[index]),
+//                         child: Text("Connect", style: buttonTextStyle,),
+//                       ),
+//                     ),
+//                   );
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
 //       ),
 //     );
 //   }
 // }
 
-
+//**********************************Updated Code********************************* */
 import 'package:ble_sdk_connection/utils/colors.dart';
 import 'package:ble_sdk_connection/utils/constants.dart';
 import 'package:flutter/material.dart';
@@ -145,59 +177,46 @@ class _MainScreenState extends State<MainScreen> {
     try {
       await platform.invokeMethod("connectDevice", {"macAddress": macAddress});
     } catch (e) {
-      print("Failed to connect: $e");
+      print("Failed to connect to device: $e");
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: themeColor,
-
-        title: Text("BLE Weight Tracker", style: appBarTitleStyle,)),
+        title: Text("BLE Weight Scale", style: appBarTitleStyle,)),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical:20,horizontal: 16 ),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Weight Data: $_weightData", style: blackTitleStyle),
-            SizedBox(height: 20,),
+          children: [
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: themeColor
-              ),
               onPressed: _startScan,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4,horizontal: 12),
-                child: Text("Start Scan", style: buttonTextStyle,),
-              ),
+              child: Text("Start Scanning"),
             ),
             Expanded(
               child: ListView.builder(
                 itemCount: _availableDevices.length,
                 itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: ListTile(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),),
-                      tileColor: wColor,
-                      title: Text(_availableDevices[index]),
-                      trailing: ElevatedButton(
-                        style: ElevatedButton.styleFrom(backgroundColor: themeColor),
-                        onPressed: () => _connectToDevice(_availableDevices[index]),
-                        child: Text("Connect", style: buttonTextStyle,),
-                      ),
+                  return ListTile(
+                    title: Text(_availableDevices[index]),
+                    trailing: ElevatedButton(
+                      onPressed: () {
+                        _connectToDevice(_availableDevices[index]);
+                      },
+                      child: Text("Connect"),
                     ),
                   );
                 },
               ),
             ),
+            Text("Weight: $_weightData"),
           ],
         ),
       ),
     );
   }
 }
+
